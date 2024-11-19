0b91185c3ca022a3296e37855d41eb213d8446b6
shafarabbani <shafarabbani12@gmail.com>
2024-11-19 17:41:44 +0700

    menambahkan pengecekan jumlah barang

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function calculate() {
+  const a = parseInt(document.getElementById("b").value) || 0;
+  const b = parseInt(document.getElementById("b").value) || 0;
+  const c = parseInt(document.getElementById("c").value) || 0;
+
+  console.log(a + b + c);
+}
